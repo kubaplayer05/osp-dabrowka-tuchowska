dfd8cd3d6294ea53702c10450b65ee8d493ecbe7
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-15 12:27:50 +0200

    delete bio and seo default components

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,12 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../layout/layout"
-import Seo from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout>
-      <Seo title="404: Not Found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
